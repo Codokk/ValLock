95fd27b838d5cd61b840a1cce6b52204f322a0e0
Cody Krist <137073012+cmkrist@users.noreply.github.com>
2024-03-26 09:23:27 -0400

    Added custom resolution support

--- main.py
+++ main.py
@@ -35,13 +35,30 @@ agents = [
     "viper",
     "yoru"
 ]
+agentBlacklist = []
+agentsperrow = 9
+removeMode = False
 data = None
 window = None
-portraitSize = 101
-startx = 782
-starty = 1070
-marginx = 11
-marginy = 12
+
+portraitSize = 106
+startx = 779
+starty = 1069
+marginx = 6
+marginy = 5
+
+defaultX = 2560
+defaultY = 1440
+
+# Adjust these values to match your screen resolution
+scaleX = pag.size().width / defaultX
+scaleY = pag.size().height / defaultY
+
+portraitSize = int(portraitSize * scaleX)
+startx = int(startx * scaleX)
+starty = int(starty * scaleY)
+marginx = int(marginx * scaleX)
+marginy = int(marginy * scaleY)
 
 def generate_portraits():
     # Start with screenshot of game
@@ -63,21 +80,43 @@ def generate_portraits_from_screenshot():
 
 def load_data(data):
     data_file_path = os.path.join(app_folder_path, "data.json")
+    try :
+        with open(data_file_path, "r") as f:
+            data = json.load(f)
+    except:
+        data = {
+            "loginBox": {
+                "x": 1279,
+                "y": 975
+            }
+        }
+        if not os.path.exists(app_folder_path):
+            os.makedirs(app_folder_path)
+        with open(data_file_path, "w") as f:
+            json.dump(data, f)
     with open(data_file_path, "r") as f:
         data = json.load(f)
     return data
 def generateLayout():
     agentsPerRow = 9
+    enabledAgents = []
+    for agent in agents:
+        if agent not in agentBlacklist:
+            enabledAgents.append(agent)
     layout = []
     row = []
     layout.append([sg.Text("Choose Your Character")])
-    for i in range(len(agents)):
+    for i in range(len(enabledAgents)):
         if i % agentsPerRow == 0 and i != 0:
             layout.append(row)
             row = []
-        row.append(sg.Button('',image_filename=imageFolder + agents[i] + ".png", key=agents[i]))
+        # if image exists, append image, else append button
+        if os.path.exists(imageFolder + enabledAgents[i] + ".png"):
+            row.append(sg.Button(enabledAgents[i],image_filename=imageFolder + enabledAgents[i] + ".png", key=enabledAgents[i]))
+        else:
+            row.append(sg.Button(enabledAgents[i], key=enabledAgents[i]))
     layout.append(row)
-    layout.append([sg.Text("Current Resolution: " + str(pag.size().width) + "x" + str(pag.size().height)), sg.Button("Open Config Folder")])
+    layout.append([sg.Text("Current Resolution: " + str(pag.size().width) + "x" + str(pag.size().height)), sg.Button("Remove Mode")])
     layout.append([sg.Button("Generate Portraits"),sg.Button("Update Positions"),sg.Button('Close')])
     return layout
 
@@ -92,10 +131,17 @@ def select_agent(agent, data):
             pag.moveTo(data['loginBox']['x'], data['loginBox']['y'], 0.1)
             pag.doubleClick(data['loginBox']['x'], data['loginBox']['y']);
             break;
+    time.sleep(3)
     window.un_hide()
     # Check for agent data
 
 def main():
+    print("Starting ValLock...")
+    # Create the app and image folderes
+    if not os.path.exists(app_folder_path):
+        os.makedirs(app_folder_path)
+    if not os.path.exists(imageFolder):
+        os.makedirs(imageFolder)
     # generate the window
     global window
     global data
@@ -104,20 +150,35 @@ def main():
     window = sg.Window('ValLock', layout)
 
     while True:
-        event, values = window.read()      
+        event, values = window.read()    
+        print(event)  
         if event == sg.WIN_CLOSED or event == 'Close':
             break
         elif event == 'Update Positions':
             print("Updating Positions")
-        elif event == 'Open Config Folder':
-            os.startfile(app_folder_path)
+        elif event == 'Remove Mode':
+            global removeMode
+            removeMode = not removeMode
+            if removeMode:
+                sg.popup("Remove Mode Enabled")
+            else:
+                sg.popup("Remove Mode Disabled")
         elif event == 'Generate Portraits':
             sg.popup("You will have 5 seconds to tab into Valorant and open the character select screen. Make sure the game is in fullscreen mode and on your primary monitor.")
             time.sleep(5)
             generate_portraits()
-            sg.popup("Reopen the app for changes to take effect.")
-            exit();
+            layout = generateLayout()
+            window.close()
+            window = sg.Window('ValLock', layout)
+            sg.popup("Portraits Generated")
         else:
-            select_agent(event, data)
+            if removeMode:
+                agentBlacklist.append(event)
+                sg.popup("Agent " + event + " added to blacklist")
+                layout = generateLayout()
+                window.close()
+                window = sg.Window('ValLock', layout)
+            else:
+                select_agent(event, data)
 
 main()
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 # ValLock
 A Valorant Instalocker with GUI 
+# How to use
+1. Open up Valorant, then open this program
+2. Go to custom game, but hold off on hitting start
+3. In this program, click "Generate Portraits", it will give you a popup saying you'll have 5 seconds to get into agent select
+4. Hit start in game, then hit ok on the popup. This is to makee sure the game has your custom resolution and agents
+5. Once the UI reloads with agent images, you should be ready to go!
